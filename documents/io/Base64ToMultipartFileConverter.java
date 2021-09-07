721428f7bb44659302104643b61267ba1423c8c2
steakliu <2319492349@qq.com>
2021-09-07 21:52:20 +0800

    IO Base64

--- documents/io/Base64ToMultipartFileConverter.java
+++ documents/io/Base64ToMultipartFileConverter.java
@@ -1,68 +1,68 @@
-package com.tongtech.huigu.converter;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-
-/**
- * Base64转Spring的MultipartFile文件
- */
-public class Base64ToMultipartFileConverter implements MultipartFile {
-
-	private final byte[] fileContent;
-
-	private final String extension;
-	private final String contentType;
-
-
-
-	public Base64ToMultipartFileConverter(String base64, String extension, String contentType) {
-		this.fileContent = Base64.getDecoder().decode(base64.getBytes(StandardCharsets.UTF_8));
-		this.extension = extension;
-		this.contentType = contentType;
-	}
-
-	@Override
-	public String getName() {
-		return "param_" + System.currentTimeMillis();
-	}
-
-	@Override
-	public String getOriginalFilename() {
-		return "file_" + System.currentTimeMillis() + extension;
-	}
-
-	@Override
-	public String getContentType() {
-		return contentType;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return fileContent == null || fileContent.length == 0;
-	}
-
-	@Override
-	public long getSize() {
-		return fileContent.length;
-	}
-
-	@Override
-	public byte[] getBytes() throws IOException {
-		return fileContent;
-	}
-
-	@Override
-	public InputStream getInputStream() throws IOException {
-		return new ByteArrayInputStream(fileContent);
-	}
-
-	@Override
-	public void transferTo(File file) throws IOException, IllegalStateException {
-		try (FileOutputStream fos = new FileOutputStream(file)) {
-			fos.write(fileContent);
-		}
-	}
-}
+//package com.tongtech.huigu.converter;
+//
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.*;
+//import java.nio.charset.StandardCharsets;
+//import java.util.Base64;
+//
+///**
+// * Base64转Spring的MultipartFile文件
+// */
+//public class Base64ToMultipartFileConverter implements MultipartFile {
+//
+//	private final byte[] fileContent;
+//
+//	private final String extension;
+//	private final String contentType;
+//
+//
+//
+//	public Base64ToMultipartFileConverter(String base64, String extension, String contentType) {
+//		this.fileContent = Base64.getDecoder().decode(base64.getBytes(StandardCharsets.UTF_8));
+//		this.extension = extension;
+//		this.contentType = contentType;
+//	}
+//
+//	@Override
+//	public String getName() {
+//		return "param_" + System.currentTimeMillis();
+//	}
+//
+//	@Override
+//	public String getOriginalFilename() {
+//		return "file_" + System.currentTimeMillis() + extension;
+//	}
+//
+//	@Override
+//	public String getContentType() {
+//		return contentType;
+//	}
+//
+//	@Override
+//	public boolean isEmpty() {
+//		return fileContent == null || fileContent.length == 0;
+//	}
+//
+//	@Override
+//	public long getSize() {
+//		return fileContent.length;
+//	}
+//
+//	@Override
+//	public byte[] getBytes() throws IOException {
+//		return fileContent;
+//	}
+//
+//	@Override
+//	public InputStream getInputStream() throws IOException {
+//		return new ByteArrayInputStream(fileContent);
+//	}
+//
+//	@Override
+//	public void transferTo(File file) throws IOException, IllegalStateException {
+//		try (FileOutputStream fos = new FileOutputStream(file)) {
+//			fos.write(fileContent);
+//		}
+//	}
+//}
